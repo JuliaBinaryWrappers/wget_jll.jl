230ff319678a25b5cf4a462eb33377962eaca99b
jlbuild <juliabuildbot@gmail.com>
2021-05-13 00:04:57 +0000

    wget_jll build 1.21.1+0

--- src/wrappers/powerpc64le-linux-gnu.jl
+++ src/wrappers/powerpc64le-linux-gnu.jl
@@ -2,10 +2,11 @@
 export wget
 
 using GnuTLS_jll
+using Nettle_jll
 JLLWrappers.@generate_wrapper_header("wget")
 JLLWrappers.@declare_executable_product(wget)
 function __init__()
-    JLLWrappers.@generate_init_header(GnuTLS_jll)
+    JLLWrappers.@generate_init_header(GnuTLS_jll, Nettle_jll)
     JLLWrappers.@init_executable_product(
         wget,
         "bin/wget",
